6e3ff77376c7f25c3496e2608dccab70fc46f636
Mohit Verma <112820522+mohitvdx@users.noreply.github.com>
2025-01-13 16:29:26 +0530

    connect remote server

--- proxy_server_with_cache.c
+++ proxy_server_with_cache.c
@@ -42,7 +42,34 @@ cache_element* head;
 int cache_size;
 
 int connectRemoteServer(char* host_addr, int port_num){
-    
+
+    int remoteSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    if(remoteSocket<0){
+        printf("Error in creating socket. \n");
+        return -1;
+    }
+
+    struct hostent *host = gethostbyname(host_addr);
+    if(host==NULL){
+        fprintf(stderr, "No such host exists.\n");
+        return -1;
+    }
+
+    struct sockaddr_in server_addr;
+
+    bzero((char*) &server_addr, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port_num);
+
+    bcopy((char*) host->h_addr, (char*)&server_addr.sin_addr.s_addr, host->h_length);
+
+    if(connect(remoteSocket, (struct sockaddr*)&server_addr, (socklen_t)sizeof(server_addr))<0){
+        fprintf(stderr, "Error in connecting !\n");
+        return -1;
+    }
+    return remoteSocket;
+
 }
 
 int handle_request(int clientSocket, ParsedRequest *request, char *tempReq){
